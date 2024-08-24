189812936d23954a8d49061890ef683457b1dbbe
MaxTever <m2308902@edu.misis.ru>
2024-08-24 15:43:16 +0300

    feat: added user and card schemes and models

---
+++ src/app.ts
@@ -0,0 +1,9 @@
+import express from 'express';
+import mongoose from 'mongoose';
+
+const app = express();
+
+const { PORT = 3000 } = process.env;
+mongoose.connect('mongodb://localhost:27017/mestodb');
+
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
---
+++ src/models/card.ts
@@ -0,0 +1,40 @@
+import { Schema, model } from 'mongoose';
+
+export interface ICard {
+  name: string;
+  link: string;
+  owner: Schema.Types.ObjectId;
+  likes: Array<Schema.Types.ObjectId>;
+  createdAt: Date;
+}
+
+const cardSchema = new Schema<ICard>({
+  name: {
+    type: String,
+    required: true,
+    minlength: 2,
+    maxlength: 30,
+  },
+  link: {
+    type: String,
+    required: true,
+  },
+  owner: {
+    type: Schema.Types.ObjectId,
+    ref: 'User',
+    required: true,
+  },
+  likes: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: 'User',
+      default: [],
+    },
+  ],
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+export default model<ICard>('card', cardSchema);
